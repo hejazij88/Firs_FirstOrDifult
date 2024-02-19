51da93c16064b39036aa1bb835c1ea9930da64a0
Mohammad Javad <126692128+hejazij88@users.noreply.github.com>
2024-02-19 17:04:04 +0330

    Add files via upload

---
+++ First_FirstODifult/First_FirstODifult/Program.cs
@@ -0,0 +1,18 @@
+﻿List<string> listName = new();
+
+//Throw Exception If Not Found
+ Console.WriteLine(listName.First());
+
+//If Don't Find Return Null
+
+Console.WriteLine(listName.FirstOrDefault());
+
+
+listName.Add("Name1");
+listName.Add("Name2");
+listName.Add("Name3");
+listName.Add("Name4");
+listName.Add("Name5");
+
+Console.WriteLine(listName.First(s =>s=="Name1" ));
+
